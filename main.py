1a551af1eb3642a46ffeaf343922a039066ef3f6
flust <254170321@qq.com>
2021-03-12 15:51:20 +0800

    0312-2

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ def train_CausEProd():
 
     # collect data
     train_data = Yahoo2(opt.s_c_data, opt.s_t_data)
-    val_data = Yahoo2(opt.test_data)
+    val_data = Yahoo1(opt.test_data)
 
     train_dataloader_s_c = DataLoader(train_data.s_c, opt.batch_size, shuffle=True)
     train_dataloader_s_t = DataLoader(train_data.s_t, opt.batch_size, shuffle=True)
@@ -187,6 +187,70 @@ def train_FFM(data, train_set, model='FM', inverse_propensity=None):
     # print("End. Best Iteration %d:  MAE = %.4f, MSE = %.4f. " % (best_iter, best_mae, best_mse))
     return best_NLL, best_AUC
 
+#
+# def train_CausE():
+#     # train model 'CausEProd'
+#     print('train_CausE begin')
+#
+#     # collect data
+#     train_data = Yahoo2(opt.s_c_data, opt.s_t_data)
+#     val_data = Yahoo2(opt.test_data)
+#
+#     train_dataloader_s_c = DataLoader(train_data.s_c, opt.batch_size, shuffle=True)
+#     train_dataloader_s_t = DataLoader(train_data.s_t, opt.batch_size, shuffle=True)
+#     # get model
+#     model = getattr(models, opt.model)(train_data.users_num, train_data.items_num,
+#                                        opt.embedding_size, opt.reg_c, opt.reg_c, opt.reg_tc,
+#                                        train_data.s_c[:, :2].tolist(), train_data.s_t[:, :2].tolist())
+#     model.to(opt.device)
+#     lr = opt.lr
+#     optimizer = model.get_optimizer(lr, opt.weight_decay)
+#
+#     best_mse = 10000.
+#     best_mae = 10000.
+#     best_iter = 0
+#
+#     # train
+#     model.train()
+#     for epoch in range(opt.max_epoch):
+#         t1 = time()
+#         for i, data in tqdm(enumerate(train_dataloader_s_c)):
+#             # train model
+#             user = data[:, 0].to(opt.device)
+#             item = data[:, 1].to(opt.device)
+#             label = data[:, 2].to(opt.device)
+#
+#             loss = model.calculate_loss(user.long(), item.long(), label.float(), control=True)
+#             optimizer.zero_grad()
+#             loss.backward()
+#             optimizer.step()
+#
+#         print('Loss:', loss.item())
+#         for i, data in tqdm(enumerate(train_dataloader_s_t)):
+#             # train model
+#             user = data[:, 0].to(opt.device)
+#             item = data[:, 1].to(opt.device)
+#             label = data[:, 2].to(opt.device)
+#
+#             loss = model.calculate_loss(user.long(), item.long(), label.float(), control=False)
+#             optimizer.zero_grad()
+#             loss.backward()
+#             optimizer.step()
+#
+#         t2 = time()
+#         print('Loss:', loss.item())
+#         if epoch % opt.verbose == 0:
+#             print('epoch', epoch)
+#             (mae, mse, rmse) = evaluate_model(model, val_data, None, opt)
+#             print('Iteration %d[%.1f s]: MAE = %.4f, MSE = %.4f, RMSE = %.4f [%.1f s]'
+#                   % (epoch, t2 - t1, mae, mse, rmse, time() - t2))
+#             if mae < best_mae:
+#                 best_mae, best_mse, best_iter = mae, mse, epoch
+#                 torch.save(model.state_dict(), str(type(model)) + "-model2.pth")
+#
+#     print("End. Best Iteration %d:  MAE = %.4f, MSE = %.4f. " % (best_iter, best_mae, best_mse))
+#     return model
+
 
 def train_New():
     def calculate_CTR(col):
@@ -232,7 +296,7 @@ def train_New():
     print('IPS: NLL:', IPS_NLL, 'AUC: ', IPS_AUC)
 
     # ------ CausE ------
-
+    # 要实现 FM-CausE 暂且搁置
 
     # ------ New(avg) ------
 
@@ -248,7 +312,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Demo of argparse")
     # parser.add_argument('--model', default='MF_Naive') # 1.0210 1.7074
     # parser.add_argument('--model', default='MF_IPS') # 0.8750 1.3433    0.8739 1.3451
-    # parser.add_argument('--model', default='CausEProd') # 1.0371 1.4658
+    # parser.add_argument('--model', default='CausEProd') # 0.85.. 1.
     parser.add_argument('--model', default='New')
 
     args = parser.parse_args()
